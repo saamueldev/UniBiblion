1c945afb9f5ed997e63c2240142f28f34a609fcf
Samuel Rocha <saamueldev@gmail.com>
2025-10-01 00:06:43 -0300

    inserido as telas.
    nessa versão, adicionei todas as telas que estavam no trello para o android studio

---
+++ UniBiblion/app/src/main/java/com/example/unibiblion/tela_Abrir_Noticia_Evento.kt
@@ -0,0 +1,20 @@
+package com.example.unibiblion
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class tela_Abrir_Noticia_Evento : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_tela_abrir_noticia_evento)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
