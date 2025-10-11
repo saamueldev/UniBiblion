d1f8daab771c2848fe12699c120076e7b2098c53
Samuel Rocha <saamueldev@gmail.com>
2025-10-11 13:22:32 -0300

    Tela de Esquecer a Senha e Nova Senha
    Tela de Esquecer a Senha e Nova Senha

--- UniBiblion/app/src/main/java/com/example/unibiblion/Tela_Esquecer_Senha.kt
+++ UniBiblion/app/src/main/java/com/example/unibiblion/Tela_Esquecer_Senha.kt
@@ -10,7 +10,11 @@ class Tela_Esquecer_Senha : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        // Carrega o layout XML da tela de recuperação (apenas e-mail)
         setContentView(R.layout.activity_tela_esquecer_senha)
+
+        // Aplica os insets da janela para lidar com barras do sistema
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
