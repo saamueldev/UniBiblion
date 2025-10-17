beb72f6124a9191c90da9bc079eed1b0e9ab0a18
Victor Viana <victor.vviana02@gmail.com>
2025-10-17 15:47:37 -0300

    Telas Informações
    Adiciona as telas 1 e 2 de informações sobre o aluguel do livro.

--- UniBiblion/app/src/main/java/com/example/unibiblion/Tela_Informacoes1.kt
+++ UniBiblion/app/src/main/java/com/example/unibiblion/Tela_Informacoes1.kt
@@ -1,20 +1,33 @@
 package com.example.unibiblion
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Tela_Informacoes : AppCompatActivity() {
+class Tela_Informacoes1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_tela_informacoes)
+        setContentView(R.layout.activity_tela_informacoes1)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val buttonConfirmar = findViewById<Button>(R.id.buttonConfirmar)
+
+        buttonConfirmar.setOnClickListener {
+
+            val intent = Intent(this, Tela_Informacoes2::class.java)
+
+            startActivity(intent)
+        }
+
+
     }
 }
