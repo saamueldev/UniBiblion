11729a3c361d4865dfbbbfcf24471f41ad363e25
Victor Viana <victor.vviana02@gmail.com>
2025-10-26 18:32:08 -0300

    Funcionamento de Botões Adm
    Adiciona o funcionamento dos botões da tela central e da tela de acervo do administrador.

--- UniBiblion/app/src/main/java/com/example/unibiblion/Adm_Tela_Central_Livraria.kt
+++ UniBiblion/app/src/main/java/com/example/unibiblion/Adm_Tela_Central_Livraria.kt
@@ -1,6 +1,8 @@
 package com.example.unibiblion
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,33 @@ class Adm_Tela_Central_Livraria : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val buttonGerenciarCatalogo = findViewById<Button>(R.id.buttonGerenciarCatalogo)
+
+        buttonGerenciarCatalogo.setOnClickListener {
+
+            val intent = Intent(this, Adm_Tela_Acervo_Livros::class.java)
+
+            startActivity(intent)
+        }
+
+        val buttonGerenciarCabines = findViewById<Button>(R.id.buttonGerenciarCabines)
+
+        buttonGerenciarCabines.setOnClickListener {
+
+            val intent = Intent(this, CabineAdminEditActivity::class.java)
+
+            startActivity(intent)
+        }
+
+        val buttonGerenciamentoGeral = findViewById<Button>(R.id.buttonGerenciamentoGeral)
+
+        buttonGerenciamentoGeral.setOnClickListener {
+
+            val intent = Intent(this, AdminDashboardActivity::class.java)
+
+            startActivity(intent)
         }
     }
 }
